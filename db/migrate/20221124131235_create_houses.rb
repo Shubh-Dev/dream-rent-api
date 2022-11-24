fdc55154a0ae277e26af40250c94eb6bfa71f072
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-24 18:21:13 +0500

    Added tables to the application

---
+++ db/migrate/20221124131235_create_houses.rb
@@ -0,0 +1,12 @@
+class CreateHouses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :houses do |t|
+      t.string :type
+      t.integer :rooms
+      t.boolean :balcony
+      t.integer :size
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_131420) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "date"
+    t.date "time"
+    t.integer "visitors"
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_appointments_on_house_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "type"
+    t.integer "rooms"
+    t.boolean "balcony"
+    t.integer "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "appointments", "houses"
+  add_foreign_key "appointments", "users"
+end
