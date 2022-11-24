a219a8fbc672b5b1081e32616947290242604eda
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-24 19:46:13 +0500

    Fixed linters

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,3 @@
 class House < ApplicationRecord
-    has_many :appointments
+  has_many :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many :appointments
+  has_many :appointments
 end
