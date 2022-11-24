dc2905f0d6efd6195a177d79b86228e7ddfe50d9
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-24 16:01:03 +0200

    add models and relations

--- db/migrate/20221124131235_create_houses.rb
+++ db/migrate/20221124131235_create_houses.rb
@@ -1,7 +1,7 @@
 class CreateHouses < ActiveRecord::Migration[7.0]
   def change
     create_table :houses do |t|
-      t.string :type
+      t.string :house_type
       t.integer :rooms
       t.boolean :balcony
       t.integer :size
--- db/migrate/20221124131420_create_appointments.rb
+++ db/migrate/20221124131420_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
       t.date :date
-      t.date :time
+      t.time :time
       t.integer :visitors
       t.references :user, null: false, foreign_key: true
       t.references :house, null: false, foreign_key: true
