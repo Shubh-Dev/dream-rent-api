d6a9b42fa17e3dcbbbb3c610f55f797faf99d54c
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-28 12:05:19 +0200

    add user register functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::UsersController < ApplicationController
 
     def register
-        @user = User.new(user_params)
-        if @user.save
-            render json: {status: 'SUCCESS', message: 'User created', data: @user}, status: :ok
-        else
-            render json: {status: 'ERROR', message: 'User not created', data: @user.errors}, status: :unprocessable_entity
+        if User.find_by(user_params) == nil
+          @user = User.new(user_params)
+          if @user.save
+              render json: {status: 'SUCCESS', message: 'User created', data: @user}, status: :ok
+          else
+              render json: {status: 'ERROR', message: 'User not created', data: @user.errors}, status: :unprocessable_entity
+          end
+        else 
+            render json: {status: 'ERROR', message: 'User already exists' }, status: :unprocessable_entity
         end
     end
+
+    
 
     def user_params
         params.require(:user).permit(:name)
