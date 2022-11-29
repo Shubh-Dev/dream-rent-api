7a48c3902eb08e5b709862b8e082a7b9a183ed6e
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-29 14:48:56 +0500

    Updated routes except for api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'houses/index'
-  get 'houses/new'
-  get 'houses/show'
-  get 'houses/create'
-  get 'houses/destroy'
-  get 'houses/update'
+  # get 'houses/index'
+  # get 'houses/new'
+  # get 'houses/show'
+  # get 'houses/create'
+  # get 'houses/destroy'
+  # get 'houses/update'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :houses
