90526839d0984b2815143514d958a75e25d7c261
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-29 15:32:26 +0500

    Fixed syntax error

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::HousesController < ApplicationController
   def index
     @houses = House.all.order(:id)
     render json: @houses.map { |house|
-                  return @house.mage.attached?
+                  return @house.image.attached?
                    house.as_json.merge(
                      image_path: url_for(house.image)
                    )
