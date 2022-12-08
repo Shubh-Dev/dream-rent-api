bf2e71f2da27a353a4c221beee6d370905146a3d
Shubh <shubh.scb@gmail.com>
2022-12-08 21:24:24 +0530

    BUG FIX

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,7 @@ class House < ApplicationRecord
   has_many :appointments
   has_one_attached :image
 
+  validates :balcony, inclusion: { in: [true, false] }
   validate :check_file_presence
 
   def check_file_presence
